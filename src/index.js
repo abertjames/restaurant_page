372503d90316ddf2a44970ebe983caec1766ef7b
abertjames <96638271+abertjames@users.noreply.github.com>
2022-08-05 22:02:20 -0400

    sets up basic structure importing images

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-console.log("Hello");
+// console.log("Hello");
 
 //import the page load function here 
 
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
+    // const element = document.createElement('div');
  
-    element.innerHTML = "Hello";
-    element.classList.add('hello');
+    // element.innerHTML = "Hello";
+    // element.classList.add('hello');
  
-    return element;
+    // return element;
+
+    const foodImage = document.createElement("img");
+    foodImage.src = 'images/the-slim-s-diner.jpeg'
+    return foodImage;
 }
  
 document.body.appendChild(component());
